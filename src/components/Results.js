270199a935f505c5d7c8454c99ce5c7ba7e20962
blangwell <b.langwell@outlook.com>
2020-09-23 13:57:11 -0700

    [Results] Populate Results component with mapped prop data, add data to App.js for testing purposes

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
 export default function Results(props) {
+  let clinicResults = props.data.map(d => {
+    return (
+      <div key={d.Id}>
+        {/* result name */}
+        <h2>{d.CtrNm}</h2>
+        {/* distance */}
+        <p>{d.Distance} Miles Away</p>
+        <p>Onsite Testing Available: {d.Covid19TestStatus}</p>
+        {/* telehealth */}
+        <p>Telehealth Available: {d.TeleHealthStatus}</p>
+        <p>Telehealth Status: {d.TelehealthText}</p>
+        {/* address */}
+        <h4>Address:</h4>
+        <p>{d.CtrAddress}</p>
+        <p>{d.CtrCity}, {d.CtrStateAbbr} {d.CtrZipcd}</p>
+        {/* phone */}
+        <h4>Phone:</h4>
+        <p>{d.CtrPhoneNum}</p>
+        <hr />
+      </div>
+    )
+  })
   return (
-    <h1>Clinics/Providers Near You</h1>
-  )
-}
+    <>
+      <h1>Clinics/Providers Near You</h1>
+      <hr />
+      {clinicResults}
+    </>
+    )
+  }
