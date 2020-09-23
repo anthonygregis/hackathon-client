17ff3e4da8ba9695ea191d78c7aaea960842072b
mac <shebah712@gmail.com>
2020-09-23 15:36:48 -0700

    [zipform] add bootstrap

--- src/components/Zipform.js
+++ src/components/Zipform.js
@@ -4,18 +4,32 @@ const Zipform = () => {
 
     return(
         <div className="zipform_box">
-            <form>
-                <label htmlFor="Zip">Zipcode</label>
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Enter zipcode here" aria-label="Your zipcode" aria-describedby="button-addon2"/>
-                    <div class="input-group-append">
-                        <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={handleSubmit}>Button</button>
+            <div className='container'>
+                <form>
+                    <label htmlFor="Zip">Zipcode</label>
+
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Enter your zipcode" aria-label="zipcode" aria-describedby="button-addon2"/>
                     </div>
-                </div>
-            </form>
+                        <br></br>
+                        <div class="form-group">
+                            <label for="radius">Radius</label>
+                            <select class="form-control" id="radius">
+                                <option>5 miles</option>
+                                <option>10 miles</option>
+                                <option>15 miles</option>
+                                <option>20 miles</option>
+                            </select>
+                        </div>
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Submit</button>
+                        </div>
 
 
-            
+                    
+
+                </form>
+            </div>
         </div>
     )
 }
