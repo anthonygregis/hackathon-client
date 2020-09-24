795b32ae23b33359c9065066c3ab49d5d3c630ab
blangwell <b.langwell@outlook.com>
2020-09-23 18:01:10 -0700

    [conflicts] Fix conflicts

--- src/components/Zipform.js
+++ src/components/Zipform.js
@@ -17,21 +17,9 @@ const Zipform = () => {
 
     return(
         <div className="zipform_box">
-<<<<<<< HEAD
-            <form>
-                <label htmlFor="Zip">Zipcode</label>
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Enter zipcode here" aria-label="Your zipcode" aria-describedby="button-addon2"/>
-                    <div class="input-group-append">
-                        {/* <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={handleSubmit}>Button</button> */}
-                    </div>
-                </div>
-            </form>
-=======
             <div className='container'>
                 <form>
                     <label htmlFor="Zip">Zipcode</label>
->>>>>>> c3babf2f7991f83a597a1fca46d066ef2dd40d87
 
                     <div class="input-group mb-3">
                         <input type="text" class="form-control" placeholder="Enter your zipcode" aria-label="zipcode" aria-describedby="button-addon2" onChange={ (e) => {setZipcode(e.target.value)}}/>
