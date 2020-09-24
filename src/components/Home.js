7f6b79739cc625d82b59fade60e913a75fd09848
blangwell <b.langwell@outlook.com>
2020-09-24 11:42:39 -0700

    [focus] Add focus to useEffect in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Zipform from './Zipform';
 
 const Home = (props) => {
 
+    useEffect(() => {
+        let landingElem = document.getElementById('landing')
+        landingElem.focus()
+    }, [])
+
     return (
-        <div>
+        <div id="landing" tabIndex="-1">
             <h1>Covid Test Finder</h1>
             <h2>Find Covid Testing Near You</h2>
             <Zipform zip={props.zip}/>
